abb6136a3e98d577032abce6a766ef5b24541032
white <shihui316@gmail.com>
2026-04-18 14:06:24 +0800

    test

---
+++ src/main.c
@@ -0,0 +1,16 @@
+
+
+#include "cub3d.h"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		exit_error("Invalid argument");
+	file_ext(argv[1]);
+
+	// check map validity
+
+	// init map
+
+	return (0);
+}
